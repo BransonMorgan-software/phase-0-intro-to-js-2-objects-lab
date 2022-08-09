38ba4f139c18f63b2b192178a07941bf1ccc6fe4
Branson Morgan <bransox@comcast.net>
2022-08-09 15:51:22 -0400

    commit tree 812bacc6cee696825f137c983016f237762e2010

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "branson morgan",
+    streetAddress: "400 mcdaniel",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+const newEmployee = updateEmployeeWithKeyAndValue(employee, streetAddress, '11 samson');
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee; 
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
